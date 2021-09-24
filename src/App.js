97f376860d4db9f1e1edae36cc2087032d33e2cd
Sahil <hanss9545@gmail.com>
2021-09-25 01:52:27 +0530

    portfolio set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import LeftSection from "./LeftSection";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container-fluid main-cont">
+          <div className="row">
+            <div className="">
+                <LeftSection></LeftSection>
+            </div>
+
+            <div className="right-section">
+
+            </div>
+          </div>
     </div>
-  );
+        
+    </>
+    
+  )
 }
 
 export default App;
