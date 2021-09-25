b4ac75d14ac962c66758ab50cfc8eb6a73badf79
Sahil <hanss9545@gmail.com>
2021-09-25 17:16:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from "react";
+import { useEffect } from "react";
 
 
 function App() {
+
+  
+
+  useEffect(()=>{
+    const menuBar=document.querySelector(".menuBar");
+    const leftCont=document.querySelector(".left-section-cont")
+    menuBar.addEventListener("click",()=>{
+      console.log(leftCont)
+        menuBar.classList.toggle("cross-menu");
+        leftCont.classList.toggle("show-left")
+    })
+  },[])
+
   return (
     <>
     <div className="container-fluid main-cont">
-          <div className="row">
-            <div className="">
+      <div className="menuBar">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+          <div className="row d-flex">
+            <div className="left-section-cont">
                 <LeftSection></LeftSection>
             </div>
 
-            <div className="right-section">
-
+            <div className="right-section-cont">
+                <RightSection></RightSection>
             </div>
           </div>
     </div>
