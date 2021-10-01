e25c8befedc56647a7f5c8412511a17b22b531ef
Sahil <hanss9545@gmail.com>
2021-10-02 00:31:33 +0530

    new Header Slider

--- src/RightSection.js
+++ src/RightSection.js
@@ -1,127 +1,64 @@
 import React, { useEffect, useState } from 'react'
-import head1 from "./images/head1.jpg";
-import head2 from "./images/head2.png";
-import head3 from "./images/head3.jpg";
 import Skills from './skills';
+import Slider from './Slider';
 
 function RightSection() {
 
-    
-    let [type,setType]=useState("");
 
-    useEffect(()=>{
-        
-       
-        const textArr=["Web Developer","Freelancer","Designer"];
-        let count =0;
-        let index=0;
-        let textString="";
-        let letter="";
+    let [type, setType] = useState("");
 
-        const typing=()=>{
+    useEffect(() => {
 
 
-            if(count===3){
-                count=0;
+        const textArr = ["Web Developer", "Freelancer", "Designer"];
+        let count = 0;
+        let index = 0;
+        let textString = "";
+        let letter = "";
+
+        const typing = () => {
+
+
+            if (count === 3) {
+                count = 0;
             }
-    
-            textString=textArr[count]
-            letter=textString.slice(0,++index);
-    
+
+            textString = textArr[count]
+            letter = textString.slice(0, ++index);
+
             setType(letter);
-            if(letter.length===textString.length){
+            if (letter.length === textString.length) {
                 count++;
-                index=0;
+                index = 0;
                 setTimeout(typing, 3000);
             }
-            else{
+            else {
                 setTimeout(typing, 300);
             }
         }
         typing();
 
 
-      let slider=document.querySelector(".slider");
-      let slide_box=document.querySelector(".slide-box");
-      let sliderDots=document.querySelectorAll(".slider .dots div");
-      let sliderWidth=slider.clientWidth;
-      console.log(sliderWidth)
-
-      let slideCount=0;
-
-      setInterval(() => {
-        if(slideCount===3){
-            slideCount=0;
-            slide_box.style.transition="none";
-        }
-        else{
-            slide_box.style.transition="all 0.5s";
-        }
-        changeSlide(slideCount);
-        dotBg(slideCount);
-        
-        slideCount++;
-        
-      }, 4000);
-
-      const changeSlide=(Num)=>{
-        slide_box.style.transform=`translateX(${ -(Num * sliderWidth)}px)`;
-      }
-
-      for(let i=0;i<sliderDots.length;i++){
-        sliderDots[i].addEventListener("click",()=>{
-            changeSlide(i);
-            dotBg(i);
-        })
-      }
-
-      const dotBg=(white)=>{
-        for(let j=0;j<sliderDots.length;j++){
-                sliderDots[j].style.background="black";
-                sliderDots[white].style.background="#fff"
-        }
-      }
-        
-
-    },[]);
+    }, []);
 
     return (
         <div className="right-section">
+
             <header>
 
-            <div className="slider">
-        <div className="slide-box">
-            <div className="slide">
-                <img src={head1} alt="" />
-            </div>
+                <Slider />
 
-            <div className="slide">
-                <img src={head2} alt="" />
-            </div>
 
-            <div className="slide">
-                <img src={head3} alt="" />
-            </div>
-        </div>
-
-        <div className="dots">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
-
-           
                 <div className="text-cont">
                     <h1>Sahil Hans</h1>
                     <h3>I am <span className="typing">{type}</span></h3>
                 </div>
 
                 <div className="profile-head">
-                    
+
                 </div>
-           
-            
+
+
             </header>
             <Skills></Skills>
         </div>
