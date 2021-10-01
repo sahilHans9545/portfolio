fec12ac449466f7806e13e030b327cd375732a45
Sahil <hanss9545@gmail.com>
2021-10-02 00:35:48 +0530

    slider

---
+++ src/Slider.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import { useEffect } from 'react/cjs/react.development'
+
+import head1 from "./images/head1.jpg";
+import head2 from "./images/head2.png";
+import head3 from "./images/head3.jpg";
+
+
+
+
+
+
+function Slider() {
+
+    useEffect(()=>{
+
+        const Slider=()=>{
+            const sliderdots=[...document.querySelectorAll(".slider-dots .dot")];
+    
+            
+               let sliderImages=document.querySelectorAll(".inner-slider .item img")
+               
+               let imageWidth=sliderImages[0].clientWidth
+            
+               const innerslider=document.querySelector(".inner-slider");
+            
+               let countSlide=0;
+            
+               const changeBg=(num)=>{
+                    innerslider.style.transform=`translateX(${-(num * imageWidth)}px)`
+               }
+            
+               const maintainSliderClasses=(num)=>{
+                for(let i=0;i<sliderdots.length;i++){
+                        sliderdots[i].classList.remove("dot-bg");
+                        sliderdots[num].classList.add("dot-bg");
+                        sliderImages[i].classList.remove('slide')
+                        sliderImages[num].classList.add('slide')
+                    }
+               }
+            
+               sliderdots.forEach((dot,ind)=>{
+                   dot.addEventListener("click",()=>{
+                    
+                    countSlide=ind;
+                       changeBg(countSlide);
+            
+                       clearInterval(sliderInterval);
+                       maintainSliderClasses(ind);
+                   })
+               })
+            
+            
+               const sliderInterval=setInterval(()=>{
+                   countSlide++
+                   if(countSlide===3){
+                       countSlide=0;
+                   }
+            
+                   maintainSliderClasses(countSlide);
+            
+            
+                   changeBg(countSlide);
+               },4000)
+        }
+
+        Slider();
+
+    },[])
+
+    return (
+        <div className="head-slider">
+
+        <div className="inner-slider">
+            <div className="item">
+                <img src={head1} alt="" />
+            </div>
+
+            <div className="item">
+                <img src={head2} alt="" />
+            </div>
+
+            <div className="item">
+                <img src={head3} alt="" />
+            </div>
+
+        </div>
+
+        <div className="slider-dots">
+            <div className="dot dot-bg"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+        </div>
+
+    </div>
+    )
+}
+
+export default Slider
---
+++ src/skills.js
@@ -0,0 +1,129 @@
+import React, { useEffect } from 'react'
+
+function Skills() {
+
+    useEffect(()=>{
+
+        window.addEventListener("scroll",()=>{
+            if(document.querySelector(".skills").getBoundingClientRect().top<400){
+                let progress=document.querySelectorAll(".progress");
+                let skillTargets=document.querySelectorAll(".skills .target");
+                progress.forEach((element,ind)=>{
+                    element.style.width=`${skillTargets[ind].innerHTML}`
+                })
+                document.querySelector(".transY").style.transform="translateY(0px)"
+                document.querySelector(".transY").style.opacity="1";
+            }
+        })
+    },[])
+
+
+    
+
+    
+    
+
+    return (
+        <section className="px-md-5 px-3 py-5 skills">
+            <h1 className="section-heading">Skills</h1>
+            <p className="my-3">Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+
+            <div className="row d-flex justify-content-between transY">
+
+                <div className="col-md-6">
+                    <div className="row">
+                        <div className="col-12 my-4">
+
+                            <div className="lang d-flex justify-content-between">
+                                <span>HTML</span>
+                                <span className="target">100%</span>
+                            </div>
+
+                            <div className="progress-bar">
+                                <div className="progress"></div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 my-4">
+                            <div className="col-12">
+
+                                <div className="lang d-flex justify-content-between">
+                                    <span>CSS</span>
+                                    <span className="target">95%</span>
+                                </div>
+
+                                <div className="progress-bar">
+                                    <div className="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="col-12 my-4">
+                            <div className="col-12">
+
+                                <div className="lang d-flex justify-content-between">
+                                    <span>javascript</span>
+                                    <span className="target">80%</span>
+                                </div>
+
+                                <div className="progress-bar">
+                                    <div className="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6">
+                    <div className="row">
+                        <div className="col-12 my-4">
+
+                            <div className="lang d-flex justify-content-between">
+                                <span>Bootstrap</span>
+                                <span className="target">95%</span>
+                            </div>
+
+                            <div className="progress-bar">
+                                <div className="progress"></div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 my-4">
+                            <div className="col-12">
+
+                                <div className="lang d-flex justify-content-between">
+                                    <span>React</span>
+                                    <span className="target">80%</span>
+                                </div>
+
+                                <div className="progress-bar">
+                                    <div className="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="col-12 my-4">
+                            <div className="col-12">
+
+                                <div className="lang d-flex justify-content-between">
+                                    <span>Figma</span>
+                                    <span className="target">75%</span>
+                                </div>
+
+                                <div className="progress-bar">
+                                    <div className="progress"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+
+export default Skills
