fad675fed9e50146fd4039e18ce996ca37a6de4d
Sahil <hanss9545@gmail.com>
2021-10-02 18:13:43 +0530

    new header

---
+++ src222/components/InnerSlider.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+
+function InnerSlider() {
+
+    const [sliderIndex, setSliderIndex] = useState(0);
+
+
+    const changeSlide = (ind) => {
+        setSliderIndex(ind)
+    }
+
+    let imgPath = process.env.PUBLIC_URL
+
+    const imgArr = [imgPath + "images/head1.jpg", imgPath + "images/head2.jpg", imgPath + "images/head3.jpg"]
+
+
+    useEffect(()=>{
+
+        let ss=0;
+
+        const interval=setInterval(() => {
+            if(ss===3){
+                ss=0
+            }
+            console.log("helo")
+            setSliderIndex(ss)
+            ss++;
+        }, 4000);
+    },[])
+
+
+
+    return (
+        <>
+            <div className="inner-slider">
+                {imgArr.map((e, index) => {
+                    return <div className={sliderIndex === index ? "slide active-slide" : "slide"} key={index}>
+                        <img src={e} alt="" />
+                    </div>
+                })}
+
+            </div>
+
+
+            <div className="dots">
+                {imgArr.map((e, index) => {
+                    return (<div className={sliderIndex === index ? "dot active-dot" : "dot"} key={index} onClick={() => {
+                        changeSlide(index)
+                    }}></div>)
+                })}
+            </div>
+
+        </>
+    )
+}
+
+export default InnerSlider
